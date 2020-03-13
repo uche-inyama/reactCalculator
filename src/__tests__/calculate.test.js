b294d08c52d148bee1d8909bfe3fab15ca93034b
uche-inyama <depoulet@yahoo.com>
2020-03-13 05:02:32 +0100

    correct eslint errors

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -1,18 +1,18 @@
 import calculate from '../logic/calculate';
 
 describe('an object', () => {
-  it('updates the properties of the object', () => {
+  it("updates the properties of the object, for buttonName '+/-'", () => {
     const obj = {
       total: 0,
       next: 0,
-      operation: '',
+      operation: '+/-',
     };
     const { total, next } = calculate(obj, '+/-');
     expect(total).toEqual(-0);
     expect(next).toEqual(-0);
   });
 
-  it('', () => {
+  it("updates the properties of the object, for buttonName '+'", () => {
     const obj = {
       total: 2,
       next: 3,
@@ -20,5 +20,35 @@ describe('an object', () => {
     };
     const { total } = calculate(obj, '+');
     expect(total).toEqual(5);
+  });
+
+  it("updates the properties of the object, for buttonName 'x'", () => {
+    const obj = {
+      total: 2,
+      next: 3,
+      operation: '+',
+    };
+    const { total } = calculate(obj, 'x');
+    expect(total).toEqual(6);
+  });
+
+  it("updates the properties of the object, for buttonName '-'", () => {
+    const obj = {
+      total: 5,
+      next: 3,
+      operation: '-',
+    };
+    const { total } = calculate(obj, '-');
+    expect(total).toEqual(2);
+  });
+
+  it("updates the properties of the object, for buttonName '÷'", () => {
+    const obj = {
+      total: 6,
+      next: 3,
+      operation: '÷',
+    };
+    const { total } = calculate(obj, '÷');
+    expect(total).toEqual(2);
   });
 });
