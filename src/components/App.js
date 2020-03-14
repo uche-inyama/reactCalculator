ada327bbaff505f9796f1478f5977a1f2ab67cc8
uche-inyama <depoulet@yahoo.com>
2020-03-14 12:12:34 +0100

    implement requirements for App component

--- src/components/App.js
+++ src/components/App.js
@@ -7,14 +7,34 @@ import calculate from '../logic/calculate';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const { total, next, operation } = this.state;
+    const data = { total, next, operation };
+
+    const result = calculate(data, buttonName);
+    this.setState({
+      total: result.total,
+      next: result.next,
+      operation: result.operation,
+    });
   }
 
   render() {
+    const { total, next } = this.state;
+    const newResult = next || total;
+    const answer = next || total ? newResult : '';
     return (
       <div className="app">
-        <Display result="" />
-        <ButtonPanel />
+        <Display result={answer} />
+        <ButtonPanel clickHandler={this.handleClick} />
       </div>
     );
   }
